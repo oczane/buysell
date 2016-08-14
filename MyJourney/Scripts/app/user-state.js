09a8368f343a40dbd2b77fb02dd03e53625be4fe
Rakesh Gupta <grakesh18@gmail.com>
2016-08-14 12:53:44 +0530

    json data and ajax. fake wait added

--- MyJourney/Scripts/app/user-state.js
+++ MyJourney/Scripts/app/user-state.js
@@ -16,24 +16,25 @@
         $("#loginLink").click(function () {
             //to-do Login dialog
         });
+
+        function Top10Adverts() {
+            $.ajax({
+                url: '/MyJourney/Advertisement/Top10Advert',
+                data: {
+                    format: 'json'
+                },
+                error: function () {
+                    $('#imgLoader').hide();
+                    $('#divTop10').html('<p>An error has occurred</p>');
+                },
+                success: function (data) {
+                    $('#imgLoader').hide();
+                    for (var i = 0; i < data.length - 1; i++) {
+                        $('#divTop10').append("<a href='/MyJourney/Advertisement/Detail/" + data[i].Title.replace(" ", "-") + "'><h4>" + data[i].Title + "</h4>" + "<img src='/MyJourney/advs/" + data[i].Image + "' width='100px' /></a>");
+                    }
+                },
+                type: 'GET'
+            });
+        }
 });
 
-function Top10Adverts() {
-    $.ajax({
-        url: '/MyJourney/Advertisement/Top10Advert',
-        data: {
-            format: 'json'
-        },
-        error: function () {
-            $('#imgLoader').hide();
-            $('#divTop10').html('<p>An error has occurred</p>');
-        },
-        success: function (data) {
-            $('#imgLoader').hide();
-            for (var i = 0; i < data.length - 1; i++) {
-                $('#divTop10').append("<a href='/MyJourney/Advertisement/Detail/" + data[i].Title.replace(" ", "-") + "'><h4>" + data[i].Title + "</h4>" + "<img src='/MyJourney/advs/" + data[i].Image + "' width='100px' /></a>");
-            }
-        },
-        type: 'GET'
-    });
-}
